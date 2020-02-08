0584325fead6c243f129a8ceb0f9889505662c2e
mposk98 <anythingand42@gmail.com>
2020-02-08 18:21:45 +0300

    fix coordinates

--- src/webgl.rs
+++ src/webgl.rs
@@ -2,27 +2,27 @@ use crate::Cell;
 
 pub fn create_vertices(width: u32, height: u32) -> Vec<f32> {
     let mut vertices = Vec::new();
-    let row_size = 2.0 / width as f32;
-    let col_size = 2.0 / height as f32;
+    let row_size = 2.0 / height as f32;
+    let col_size = 2.0 / width as f32;
     for i in 0..height {
-        let row_north = (i as f32) * row_size - 1.0;
-        let row_south = ((i + 1) as f32) * row_size - 1.0;
+        let row_north = -((i as f32) * row_size - 1.0);
+        let row_south = -(((i + 1) as f32) * row_size - 1.0);
         for j in 0..width {
             let column_east = ((j + 1) as f32) * col_size - 1.0;
             let column_west = (j as f32) * col_size - 1.0;
-            vertices.push(row_north);
             vertices.push(column_west);
             vertices.push(row_north);
             vertices.push(column_east);
-            vertices.push(row_south);
+            vertices.push(row_north);
             vertices.push(column_east);
-
+            vertices.push(row_south);
+            
+            vertices.push(column_west);
             vertices.push(row_north);
             vertices.push(column_west);
             vertices.push(row_south);
-            vertices.push(column_west);
-            vertices.push(row_south);
             vertices.push(column_east);
+            vertices.push(row_south);
         }
     }
     vertices
